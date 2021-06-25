076798bf206a476d52b6fdbc77b33315baef0384
farahnush <farahnush.nazarova@gmail.com>
2021-06-25 12:29:21 +0500

    sync

---
+++ localhost/HomeWork_Academy/HomeWork 3/index.php
@@ -0,0 +1,12 @@
+<?php
+/*$counter = 0;
+if (isset($_COOKIE['counter'])){
+    $counter = $_COOKIE['counter'];
+    $counter++;
+}
+setcookie('counter', $counter, time() +3600);
+echo $counter;*/
+/*session_start();
+$counter = $_SESSION['counter'] ?? 0;
+$counter++;
+$_SESSION['counter']*/
